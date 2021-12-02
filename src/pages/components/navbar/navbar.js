cb96d7b83310396f109ee16bc240576c549731ab
norvalbv <benjinorval@gmail.com>
2021-12-02 21:09:29 +0000

    fully fixed contact popup functionality

--- src/pages/components/navbar/navbar.js
+++ src/pages/components/navbar/navbar.js
@@ -17,8 +17,7 @@ const NavBar = ({openmenu, menuOpen, opencontact, contactOpen}) => {
                         <li className="nav-item"><a href="#about">About Me</a></li>
                         <li className="nav-item"><a href="#my-work">My Work</a></li>
                         <li className="nav-item"><a href="#skills">Skills</a></li>
-                        <li onClick={opencontact}>Contact{contactOpen ? <ContactPopUp className="nav-item" opencontact={opencontact} contactOpen={contactOpen} /> : null}</li>
-                        {/* <ContactPopUp className="nav-item" opencontact={opencontact} contactOpen={contactOpen}/> */}
+                        <li onClick={opencontact}>Contact</li>
                         <li className="nav-item"><a href={pdfCV} target="_blank">Download CV</a></li>
                     </ul>
                 </div>
@@ -35,13 +34,13 @@ const NavBar = ({openmenu, menuOpen, opencontact, contactOpen}) => {
                             <li className="hamburger-item"><a href="#about">About Me</a></li>
                             <li className="hamburger-item"><a href="#my-work">My Work</a></li>
                             <li className="hamburger-item"><a href="#skills">Skills</a></li>
-                            <li onClick={opencontact}>Contact{contactOpen ? <ContactPopUp className="nav-item" opencontact={opencontact} contactOpen={contactOpen} /> : null}</li>
-                            {/* <ContactPopUp className="hamburger-item" opencontact={opencontact} contactOpen={contactOpen}/> */}
+                            <li onClick={opencontact}>Contact</li>
                             <li className="hamburger-item"><a href={pdfCV} target="_blank">Download CV</a></li>
                         </ul>
                     </div>
                 </div>
             </nav>
+            {contactOpen ? <ContactPopUp className="nav-item" opencontact={opencontact} contactOpen={contactOpen} /> : null}
         </div>
     )
 };
