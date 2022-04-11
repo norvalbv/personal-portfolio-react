0f396dd5619fd75ba39501759793db73f60f0ea7
Benjamin Norval <79381943+norvalbv@users.noreply.github.com>
2022-04-11 23:35:23 +0100

    Update contact-me.jsx

--- src/components/contact-me/contact-me.jsx
+++ src/components/contact-me/contact-me.jsx
@@ -56,8 +56,8 @@ const ContactMe = () => {
       </form>
       <p style={{ fontSize: "14px" }}>Reach me on my socials...</p>
       <ul id="socials">
-        <li>GitHub</li>
-        <li>Linked In</li>
+        <li><a href="https://github.com/norvalbv/" target="_blank" rel="noopener norefferer">GitHub</a></li>
+        <li><a href="" target="_blank" rel="noopener norefferer">Linked In</a></li>
       </ul>
     </div>
   );
